27285c6dae2d4938b9a8334d0316ab3fe7da40b8
Frank Falor <ffalor@deloitte.com>
2020-06-30 12:02:50 -0500

    add azurerm resources

--- terrascript/data/azurerm.py
+++ terrascript/data/azurerm.py
@@ -3,4 +3,12 @@
 import terrascript
 
 
-__all__ = []
+class azurerm_subnet(terrascript.Data):
+    pass
+
+
+class azurerm_resource_group(terrascript.Data):
+    pass
+
+
+__all__ = ["azurerm_subnet", "azurerm_resource_group"]
--- terrascript/resource/azurerm.py
+++ terrascript/resource/azurerm.py
@@ -3,4 +3,35 @@
 import terrascript
 
 
-__all__ = []
+class azurerm_network_interface(terrascript.Resource):
+    pass
+
+
+class azurerm_windows_virtual_machine(terrascript.Resource):
+    pass
+
+
+class azurerm_linux_virtual_machine(terrascript.Resource):
+    pass
+
+
+class azurerm_network_interface_security_group_association(terrascript.Resource):
+    pass
+
+
+class azurerm_network_security_group(terrascript.Resource):
+    pass
+
+
+class azurerm_virtual_machine_extension(terrascript.Resource):
+    pass
+
+
+__all__ = [
+    "azurerm_network_interface",
+    "azurerm_windows_virtual_machine",
+    "azurerm_linux_virtual_machine",
+    "azurerm_network_security_group",
+    "azurerm_network_interface_security_group_association",
+    "azurerm_virtual_machine_extension"
+]
